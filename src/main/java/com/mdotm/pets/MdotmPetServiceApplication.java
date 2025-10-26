281becb8a59dacf53bf64e3dd2b7fe18740a9243
Emanuel Covelli <emanuel.covelli@netservice.eu>
2025-10-26 09:03:12 +0100

    chore: bootstrap Spring Boot 3 Gradle project (web, validation, data-jpa, H2) via Initializr

---
+++ src/main/java/com/mdotm/pets/MdotmPetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdotm.pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdotmPetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdotmPetServiceApplication.class, args);
+	}
+
+}
